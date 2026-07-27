362c59f73faa6d884d25a3577d22fdd521619b44
Willy Morales Jiménez <113004978+WillyMJ04@users.noreply.github.com>
2026-07-26 22:10:59 -0600

    Implement Intersection Observer for product cards

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const cards = document.querySelectorAll('.product-card');
 
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add('show');
+
+        }
+
+    });
+
+},{
+    threshold:0.15
+});
+
+cards.forEach(card => {
+
+    observer.observe(card);
+
+});
